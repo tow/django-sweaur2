b951f4b73ffd097f9197b4cb00e38133335d3997
Toby White <tow@eaddrinu.se>
2011-02-17 17:27:38 +0000

    Add tests for bearer/mac servers

--- django_sweaur2/test_sweaur2_paths.py
+++ django_sweaur2/test_sweaur2_paths.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 
-from sweaur2.constructor_tests import constructor_for_bearer_checks, constructor_for_token_endpoint
+from sweaur2.constructor_tests import constructor_for_bearer_checks, constructor_for_mac_checks, constructor_for_token_endpoint
 
 from .client import DjangoClientStore
 from .models import DjangoTokenStore
 
-globals().update(constructor_for_token_endpoint(DjangoTokenStore(), DjangoClientStore()))
 globals().update(constructor_for_bearer_checks(DjangoTokenStore(), DjangoClientStore()))
+globals().update(constructor_for_mac_checks(DjangoTokenStore(), DjangoClientStore()))
+globals().update(constructor_for_token_endpoint(DjangoTokenStore(), DjangoClientStore()))
